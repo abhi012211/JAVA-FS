0177f91c508afc7592762ee6f1454e0be43db504
ABHINAV@0122 <131744556+abhi012211@users.noreply.github.com>
2023-11-06 12:19:49 +0530

    Update prime_rec.java

--- prime_rec.java
+++ prime_rec.java
@@ -6,14 +6,11 @@ public class even_rec {
 	public static void main(String[] args) {
 		Scanner scn=new Scanner(System.in);
 		int n=scn.nextInt();
-//		if()
-//		int count=0;
-		rec(n,1,0);
-//		if(isPrime(n,2)) {
-//			System.out.println("prime");
-//		}else {
-//			System.out.println("not prime");
-//		}
+		if(isPrime(n,2)) {
+			System.out.println("prime");
+		}else {
+			System.out.println("not prime");
+		}
 	}
 	public static boolean isPrime(int n,int i) {
 		if(i>n/2) {
@@ -24,19 +21,4 @@ public class even_rec {
 		}
 		return isPrime(n,i+1);
 	}
-	public static void  rec(int n,int i,int count) {
-		if(i<=n) {
-			return;
-		}
-		if(count==2) {
-			System.out.println("prime");
-		}
-		if(i%2!=0) {
-			rec(n,i+1,count+1);
-		}else {
-			rec(n,i+1,count);
-		}
-		
-	}
-
 }
